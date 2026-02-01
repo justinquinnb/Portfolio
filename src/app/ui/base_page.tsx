d6595a22296a604af8b472790048340ae0251cc5
Justin Quinn <67286549+justinquinnb@users.noreply.github.com>
2026-02-01 00:05:08 -0500

    Checkpoint commit
    -Commit before near-clean restart

---
+++ src/app/ui/base_page.tsx
@@ -0,0 +1,7 @@
+import Navbar from "@/app/ui/navbar";
+
+export default function BasePage() {
+  return (
+      <Navbar />
+  )
+}
---
+++ src/app/ui/social_icon.tsx
@@ -0,0 +1,5 @@
+export default function SocialIcon() {
+  return (
+      <p>Test</p>
+  )
+}
