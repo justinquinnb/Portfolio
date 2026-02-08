cd7436e49107cca5f50afbfbd8902e8cfceae2b8
Justin Quinn <67286549+justinquinnb@users.noreply.github.com>
2026-02-07 21:13:13 -0500

    Add title to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import Link from "next/link";
+import type { Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home | JQB Portfolio'
+}
 
 export default function Home() {
   return (
