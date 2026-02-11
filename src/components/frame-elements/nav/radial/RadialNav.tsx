0897a4683e877cabc55718d92d4f04120e391883
Justin Quinn <67286549+justinquinnb@users.noreply.github.com>
2026-02-11 16:17:40 -0500

    Implement programmatic radial nav item positioning

--- src/components/frame-elements/nav/radial/RadialNav.tsx
+++ src/components/frame-elements/nav/radial/RadialNav.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, {useRef} from "react";
+import React, {CSSProperties, useEffect, useRef, useState} from "react";
 import styles from "./radialnav.module.css";
 import {useOnClickOutside} from 'usehooks-ts'
 import {usePathname, useRouter} from "next/navigation";
@@ -11,7 +11,7 @@ import type {RadialNavItem} from "@/types/radial-nav-item";
  * @constructor
  */
 export default function RadialNav({ className }: { className?: string }) {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [menuGroup, setMenuGroup] = React.useState("main");
   const menuRef = useRef<HTMLDivElement>(null!);
   const router = useRouter();
@@ -30,7 +30,7 @@ export default function RadialNav({ className }: { className?: string }) {
 
   // Ensure the correct group is displayed on menu open
   const currentPath = usePathname();
-  React.useEffect(() => {
+  useEffect(() => {
     if (currentPath.startsWith("/about") || currentPath === "/contact") {
       setMenuGroup("about");
     } else if (currentPath.startsWith("/my-work")) {
@@ -42,40 +42,70 @@ export default function RadialNav({ className }: { className?: string }) {
 
   // Page group definitions
   const universalGroupItems: RadialNavItem[] = [
-    {label: "Other Pages", iconName: "arrow_top_left", onClick: () => setMenuGroup("main"),
+    {label: "Categories", iconName: "arrow_top_left", onClick: () => setMenuGroup("main"),
       actionDesc: "View all page groups"},
   ];
 
   const mainGroup: RadialNavItem[] = [
-    {label: "Home", iconName: "house", onClick: () => router.push("/"),
-      actionDesc: "Go to homepage"},
     {label: "About", iconName: "article_person", onClick: () => setMenuGroup("about"),
       actionDesc: "View page selection group for biography, resume, and contact pages"},
+    {label: "Home", iconName: "house", onClick: () => router.push("/"),
+      actionDesc: "Go to homepage"},
     {label: "My Work", iconName: "gallery_thumbnail", onClick: () => setMenuGroup("work"),
       actionDesc: "View page selection group for my software, graphic design, photography, and music work"}
   ]
 
   const aboutGroup: RadialNavItem[] = [
+    ...universalGroupItems,
     {label: "Biography", iconName: "badge", onClick: () => router.push("/about/bio"),
       actionDesc: "Go to biography page"},
     {label: "Resume", iconName: "description", onClick: () => router.push("/about/resume"),
       actionDesc: "Go to resume page"},
     {label: "Contact", iconName: "mail", onClick: () => router.push("/contact"),
-      actionDesc: "Go to contact page"},
-    ...universalGroupItems
+      actionDesc: "Go to contact page"}
   ];
 
   const workGroup: RadialNavItem[] = [
+    ...universalGroupItems,
+    {label: "Photos", iconName: "photo_camera", onClick: () => router.push("/my-work/photos"),
+      actionDesc: "Go to my photos page"},
     {label: "Software", iconName: "terminal", onClick: () => router.push("/my-work/software"),
       actionDesc: "Go to my software projects page"},
     {label: "Graphics", iconName: "design_services", onClick: () => router.push("/my-work/graphics"),
       actionDesc: "Go to my graphics page"},
-    {label: "Photos", iconName: "photo_camera", onClick: () => router.push("/my-work/photos"),
-      actionDesc: "Go to my photos page"},
     {label: "Music", iconName: "music_note", onClick: () => router.push("/my-work/music"),
-      actionDesc: "Go to my music page"},
-    ...universalGroupItems
+      actionDesc: "Go to my music page"}
   ];
+
+  function findNavPlacement(
+      index: number, totalItems: number, distance: number, maxSpan: number,
+      origin: {x: number, y: number}): {x: string, y: string}
+  {
+    if (index == 0 && totalItems > 3) {
+      console.log(`Calculated position: x: 0, y: 0`);
+      return {x: "50%", y: "55%"};
+    }
+
+    const largerAngle: number = 90 + (maxSpan / 2);
+    const smallerAngle: number = 90 - (maxSpan / 2);
+    const numRadialItems: number = totalItems > 3 ? totalItems - 2 : totalItems - 1;
+    const angleIncrements: number = (largerAngle - smallerAngle) / (numRadialItems);
+    console.log(`AngleIncrements: ${angleIncrements}`);
+
+    const indexShift: number = totalItems > 3 ? 1 : 0;
+    const angleInRadians = (largerAngle - (angleIncrements * (index - indexShift))) * Math.PI / 180;
+    const x = (distance * Math.cos(angleInRadians));
+    const y = (distance * Math.sin(angleInRadians));
+
+    console.log(`Calculated position: x: ${x}, y: ${y}`);
+
+    return {x: `calc(${x}px + 50%)`, y: `calc(${y}px + 50%)`};
+  }
+
+  // Int is rem
+  const distanceFromCenter = 110;
+  const maxSpan = 160;
+  const origin = {x: 500, y: 500};
 
   return (
       <nav className={`${styles.radialNav} ${className}`}>
@@ -86,19 +116,34 @@ export default function RadialNav({ className }: { className?: string }) {
         ><span className={`material-symbols-sharp ${styles.menuIcon}`}>menu</span></button>
         <div className={`${styles.menu} ${isOpen ? styles.isOpen : ''}`} ref={menuRef}>
           <div className={`${styles.menuGroup} ${styles.main} ${menuGroup === "main" ? styles.isSelected : ""}`}>
-            {mainGroup.map((item) =>
-                <RadialNavItem key={item.label} navItem={item} className={item.label.toLowerCase()}/>
-            )}
+            {mainGroup.map((item: RadialNavItem, index: number, list: RadialNavItem[]) => {
+              const position: {x: string, y: string} = findNavPlacement(
+                  index, list.length, distanceFromCenter, maxSpan, origin);
+              return (
+                  <RadialNavItem key={item.label} navItem={item} inlineStyles={{left: position.x, bottom: position.y}}
+                                 className={item.label.toLowerCase().replace(" ", "")}/>
+              )
+            })}
           </div>
           <div className={`${styles.menuGroup} ${styles.about} ${menuGroup === "about" ? styles.isSelected : ""}`}>
-            {aboutGroup.map((item) =>
-                <RadialNavItem key={item.label} navItem={item} className={item.label.toLowerCase()}/>
-            )}
+            {aboutGroup.map((item: RadialNavItem, index: number, list: RadialNavItem[]) => {
+              const position: {x: string, y: string} = findNavPlacement(
+                  index, list.length, distanceFromCenter, maxSpan, origin);
+              return (
+                  <RadialNavItem key={item.label} navItem={item} inlineStyles={{left: position.x, bottom: position.y}}
+                                 className={item.label.toLowerCase().replace(" ", "")}/>
+              )
+            })}
           </div>
           <div className={`${styles.menuGroup} ${styles.myWork} ${menuGroup === "work" ? styles.isSelected : ""}`}>
-            {workGroup.map((item) =>
-                <RadialNavItem key={item.label} navItem={item} className={item.label.toLowerCase()}/>
-            )}
+            {workGroup.map((item: RadialNavItem, index: number, list: RadialNavItem[]) => {
+              const position: {x: string, y: string} = findNavPlacement(
+                  index, list.length, distanceFromCenter, maxSpan, origin);
+              return (
+                  <RadialNavItem key={item.label} navItem={item} inlineStyles={{left: position.x, bottom: position.y}}
+                                 className={item.label.toLowerCase().replace(" ", "")}/>
+              )
+            })}
           </div>
         </div>
       </nav>
@@ -106,11 +151,11 @@ export default function RadialNav({ className }: { className?: string }) {
 }
 
 function RadialNavItem(
-    {navItem, className}:
-    {navItem: RadialNavItem; className?: string})
+    {navItem, className, inlineStyles}:
+    {navItem: RadialNavItem; className?: string; inlineStyles?: CSSProperties})
 {
   return (
-      <button className={`${styles.navItem} ${className}`} onClick={navItem.onClick} aria-label={navItem.actionDesc}>
+      <button className={`${styles.navItem} ${className}`} style={inlineStyles} onClick={navItem.onClick} aria-label={navItem.actionDesc}>
         <span className={`${styles.navItemIcon} material-symbols-sharp`}>{navItem.iconName}</span>
         <p>{navItem.label}</p>
       </button>
