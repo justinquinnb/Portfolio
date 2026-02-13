c27b800367a1623ca839fdefdcc827812e34c4cd
Justin Quinn <67286549+justinquinnb@users.noreply.github.com>
2026-02-12 23:44:48 -0500

    Add redirects for nested pages
    -Shorten URLs for common pages that are nested by adding permanent redirects

--- next.config.ts
+++ next.config.ts
@@ -6,3 +6,40 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/resume',
+        destination: '/about/resume',
+        permanent: true,
+      },
+      {
+        source: '/bio',
+        destination: '/about/bio',
+        permanent: true,
+      },
+      {
+        source: '/software',
+        destination: '/my-work/software',
+        permanent: true,
+      },
+      {
+        source: '/graphics',
+        destination: '/my-work/graphics',
+        permanent: true,
+      },
+      {
+        source: '/music',
+        destination: '/my-work/music',
+        permanent: true,
+      },
+      {
+        source: '/photos',
+        destination: '/my-work/photos',
+        permanent: true,
+      }
+    ]
+  },
+}
