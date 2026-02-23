90f28327ff6bc0d1a644014dd7d23f4c2ae040ed
Justin Quinn <67286549+justinquinnb@users.noreply.github.com>
2026-02-22 23:17:34 -0500

    Refactor homepage intro to share single source of text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,14 @@ export const metadata: Metadata = {
 
 export default function Home() {
   const logo = <Image src={darkIcon} alt={"Justin Quinn logo"} />;
+  const displayText = "I’m a full-stack developer by day and a digital creator by night. From an early age, I’ve had one main goal: to innovate and elevate the world in all that I do."
 
   return (
       <>
         <PageHeader img={heroImage}
                     imgAlt={"Justin and team leading a DevDogs club meeting in a full auditorium"}
                     backgroundText={"Justin Quinn"} foregroundText={"Hey,\nI'm Justin"}
-                    subtitle={"A creator, leader, and innovator in Metro ATL"}
+                    subtitle={"A creator, leader, and innovator in Metro ATL."}
         >
           {logo}
         </PageHeader>
@@ -30,13 +31,11 @@ export default function Home() {
               <Image src={meImage} alt={"A stylized photo of Justin"}/>
               <div className={styles.introDecorText}>
                 <h2>I seek <span className={"emphasis"}>impact</span>...</h2>
-                <p className={"display-text"}>I’m a full-stack developer by day and a full-on creative
-                  by night. From an early age, I’ve had one main goal: to innovate and inspire in all that I do.</p>
+                <p className={"display-text"}>{displayText}</p>
                 <Link className={"link-button"} href={"/about/bio"}>More About Me</Link>
               </div>
             </div>
-            <p className={"display-text"}>I’m a full-stack developer by day and a full-on creative
-              by night. From an early age, I’ve had one main goal: to innovate and inspire in all that I do.</p>
+            <p className={"display-text"}>{displayText}</p>
           </section>
         </main>
       </>
